cc8bb4270a64c05ca0ed9057eec89307ac67aa3f
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-03-27 11:44:46 +0300

    fix15

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -24,7 +24,7 @@ export class AuthController {
                     }
                 );
 
-                res.sendStatus(200).json({
+                res.status(200).json({
                     "accessToken": accessToken
                 })
             }
